17e8bb4c43203d4eb2a1cb9c07cfd17d9d9aef06
Oleksandr Prypkhan <o.prypkhan@tenantcloud.com>
2021-02-20 17:32:57 +0200

    refactor: Drop ramsey/uuid dev dependency

--- tests/CallableLazyTest.php
+++ tests/CallableLazyTest.php
@@ -1,18 +1,29 @@
 <?php
 
-use Ramsey\Uuid\Uuid;
-use Ramsey\Uuid\UuidInterface;
 use TenantCloud\Standard\Lazy\CallableLazy;
 use function TenantCloud\Standard\Lazy\lazy;
 
-test('only resolves once', function () {
-	$lazy = new CallableLazy(fn () => Uuid::uuid6());
+test('only resolves once', function ($value) {
+	$hasCallbackExecuted = false;
+	$lazy = new CallableLazy(function () use ($value, &$hasCallbackExecuted) {
+		expect($hasCallbackExecuted)->toBeFalse();
+
+		$hasCallbackExecuted = true;
+
+		return $value;
+	});
 
 	expect($lazy->isInitialized())->toBeFalse();
-	expect($resolved = $lazy->value())->toBeInstanceOf(UuidInterface::class);
+	expect($resolved = $lazy->value())->toBe($value);
 	expect($lazy->isInitialized())->toBeTrue();
 	expect($lazy->value())->toBe($resolved);
-});
+})->with([
+	null,
+	false,
+	true,
+	mt_rand(),
+	new stdClass(),
+]);
 
 test('function creates a callable lazy', function () {
 	$lazy = lazy(fn () => 123);
