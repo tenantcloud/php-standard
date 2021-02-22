2cc9c70dad68ef01bd9e4b291e2eac2a274e66ef
oprypkhantc <54406427+oprypkhantc@users.noreply.github.com>
2021-02-22 12:03:42 +0200

    feature: ArrayValuesIterator (#1)

---
+++ src/TenantCloud/Standard/Iterators/ArrayValuesIterator.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace TenantCloud\Standard\Iterators;
+
+use Iterator;
+
+/**
+ * Similar to {@see \ArrayIterator}, except it's specifically made for single-dimensional (value) arrays.
+ * This means we can now operate with indices and insert (merge) items in between without ruining the interface.
+ *
+ * @template T
+ * @implements Iterator<T>
+ */
+class ArrayValuesIterator implements Iterator
+{
+	private int $position;
+
+	/** @var T[] */
+	private array $items;
+
+	/**
+	 * @param T[] $items
+	 */
+	public function __construct(array $items = [])
+	{
+		$this->items = $items;
+
+		$this->rewind();
+	}
+
+	/**
+	 * Merges new elements at given position.
+	 *
+	 * @param T[] $values
+	 */
+	public function merge(array $values, int $at): void
+	{
+		array_splice($this->items, $at, 0, $values);
+	}
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public function current()
+	{
+		return $this->items[$this->position];
+	}
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public function next(): void
+	{
+		$this->position++;
+	}
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public function key()
+	{
+		return $this->position;
+	}
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public function valid(): bool
+	{
+		return isset($this->items[$this->position]);
+	}
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public function rewind(): void
+	{
+		$this->position = 0;
+	}
+}
