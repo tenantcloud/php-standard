c2e306c6ff09652ad0bc56b2834bcc4f90d7051c
Oleksandr Prypkhan <o.prypkhan@tenantcloud.com>
2024-03-27 15:29:26 +0200

    fix: Missing static constructor loader in ClassLoader::getRegisteredLoaders()

--- src/StaticConstructor/StaticConstructorLoaderRegisterer.php
+++ src/StaticConstructor/StaticConstructorLoaderRegisterer.php
@@ -24,7 +24,14 @@ class StaticConstructorLoaderRegisterer
 		self::$composerClassLoader = self::findComposerLoader();
 		self::$loader = new StaticConstructorLoader(self::$composerClassLoader);
 
-		replace_spl_autoloader([self::$composerClassLoader, 'loadClass'], [self::$loader, 'loadClass']);
+		replace_spl_autoloader(
+			[self::$composerClassLoader, 'loadClass'],
+			function () {
+				assert(self::$loader !== null);
+
+				self::$loader->register(true);
+			},
+		);
 	}
 
 	public static function unregister(): void
@@ -35,7 +42,15 @@ class StaticConstructorLoaderRegisterer
 
 		assert(self::$composerClassLoader !== null);
 
-		replace_spl_autoloader([self::$loader, 'loadClass'], [self::$composerClassLoader, 'loadClass'], false);
+		replace_spl_autoloader(
+			[self::$loader, 'loadClass'],
+			function () {
+				assert(self::$composerClassLoader !== null);
+
+				self::$composerClassLoader->register(true);
+			},
+			false
+		);
 
 		self::$loader = null;
 	}
@@ -46,10 +61,6 @@ class StaticConstructorLoaderRegisterer
 	public static function findComposerLoader(): ClassLoader
 	{
 		$loaders = spl_autoload_functions();
-
-		if ($loaders === false) {
-			throw new StaticConstructorInvalidUsageException('Autoload stack is not activated.');
-		}
 
 		foreach ($loaders as $loader) {
 			if (is_array($loader) && $loader[0] instanceof ClassLoader) {
